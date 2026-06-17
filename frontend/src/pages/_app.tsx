b23e6ffb3f5316f0730fb22da3d444040e7ca8f7
Yashwanth <yashwanthsuresh8@gmail.com>
2026-06-17 09:29:26 +0530

    resolve the hydration error

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -1,20 +1,36 @@
+import type { AppProps } from "next/app";
+import { CacheProvider } from "@emotion/react";
+import { ThemeProvider, CssBaseline } from "@mui/material";
+import { useMemo } from "react";
+
+import createEmotionCache from "@/createEmotionCache";
+import theme from "@/theme";
+
 import AdminLayout from "@/layouts/AdminLayout";
 import "@/styles/globals.css";
-import type { AppProps } from "next/app";
 import { useRouter } from "next/router";
 
 export default function App({ Component, pageProps }: AppProps) {
   const router = useRouter();
 
+  const emotionCache = useMemo(() => createEmotionCache(), []);
+
   const isAdminRoute = router.pathname.startsWith("/admin");
 
-  if (isAdminRoute) {
-    return (
-      <AdminLayout>
-        <Component {...pageProps} />
-      </AdminLayout>
-    );
-  }
+  const page = isAdminRoute ? (
+    <AdminLayout>
+      <Component {...pageProps} />
+    </AdminLayout>
+  ) : (
+    <Component {...pageProps} />
+  );
 
-  return <Component {...pageProps} />;
+  return (
+    <CacheProvider value={emotionCache}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        {page}
+      </ThemeProvider>
+    </CacheProvider>
+  );
 }
